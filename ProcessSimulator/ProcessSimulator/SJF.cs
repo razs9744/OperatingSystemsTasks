2167ceb6874ab23ae11319499381d679f3feb31e
razs9744 <razs9744@gmail.com>
2019-12-31 20:44:28 +0200

    final version of the task

--- ProcessSimulator/ProcessSimulator/SJF.cs
+++ ProcessSimulator/ProcessSimulator/SJF.cs
@@ -11,81 +11,74 @@ namespace ProcessSimulator
         public override void Schedule(SimulatorModel simulator)
         {
             //sort in descending order 
-            Array.Sort(simulator.ProcessArrivingTime, (x, y) => x.Item1.CompareTo(y.Item2));
+            Array.Sort(simulator.ProcessArrivingTime, (x, y) => x.Item1.CompareTo(y.Item1));
 
             int currentTime = simulator.ProcessArrivingTime[0].Item1;
             int[] restBurstTime = new int[simulator.NumProcBigThanZero];
             int[] waitingTime = new int[simulator.NumProcBigThanZero];
 
-
-
             for (int i = 0; i < simulator.NumProcBigThanZero; i++)
                 restBurstTime[i] = simulator.ProcessArrivingTime[i].Item2;
 
-            int complete = 0, t = simulator.ProcessArrivingTime[0].Item1, minm = int.MaxValue;
-            int shortest = 0, finish_time;
-            bool check = false;
+            int completeProc = 0, totalRunningTime = simulator.ProcessArrivingTime[0].Item1, minRemainTime = int.MaxValue;
+            int shortestProcIndex = 0, finishTime;
+            bool checkIfProcessFinished = false;
 
-            // Process until all processes gets 
-            // completed 
-            while (complete != simulator.NumProcBigThanZero)
+            // Process until all processes gets completed 
+            while (completeProc != simulator.NumProcBigThanZero)
             {
-                // Find process with minimum 
-                // remaining time among the 
-                // processes that arrives till the 
-                // current time` 
-                for (int j = 0; j < simulator.NumProcBigThanZero; j++)
+                // Find process with minimum remaining time
+                for (int i = 0; i < simulator.NumProcBigThanZero; i++)
                 {
-                    if ((simulator.ProcessArrivingTime[j].Item1 <= t) &&
-                    (restBurstTime[j] < minm) && restBurstTime[j] > 0)
+                    if ((simulator.ProcessArrivingTime[i].Item1 <= totalRunningTime) &&
+                    (restBurstTime[i] < minRemainTime) && restBurstTime[i] > 0)
                     {
-                        minm = restBurstTime[j];
-                        shortest = j;
-                        check = true;
+                        minRemainTime = restBurstTime[i];
+                        shortestProcIndex = i;
+                        checkIfProcessFinished = true;
                     }
                 }
 
-                if (check == false)
+                if (checkIfProcessFinished == false)
                 {
-                    t++;
+                    totalRunningTime++;
                     continue;
                 }
 
                 // Reduce remaining time by one 
-                restBurstTime[shortest]--;
+                restBurstTime[shortestProcIndex]--;
 
                 // Update minimum 
-                minm = restBurstTime[shortest];
-                if (minm == 0)
-                    minm = int.MaxValue;
+                minRemainTime = restBurstTime[shortestProcIndex];
+                if (minRemainTime == 0)
+                    minRemainTime = int.MaxValue;
 
                 // If a process gets completely 
                 // executed 
-                if (restBurstTime[shortest] == 0)
+                if (restBurstTime[shortestProcIndex] == 0)
                 {
+                    // Increment completeProc 
+                    completeProc++;
+                    checkIfProcessFinished = false;
 
-                    // Increment complete 
-                    complete++;
-                    check = false;
-
-                    // Find finish time of current 
-                    // process 
-                    finish_time = t + 1;
+                    // Find finish time of current process
+                    finishTime = totalRunningTime + 1;
 
                     // Calculate waiting time 
-                    waitingTime[shortest] = finish_time -
-                                simulator.ProcessArrivingTime[shortest].Item2 -
-                                simulator.ProcessArrivingTime[shortest].Item1;
+                    waitingTime[shortestProcIndex] = finishTime -
+                                simulator.ProcessArrivingTime[shortestProcIndex].Item2 -
+                                simulator.ProcessArrivingTime[shortestProcIndex].Item1;
 
-                    if (waitingTime[shortest] < 0)
-                        waitingTime[shortest] = 0;
+                    if (waitingTime[shortestProcIndex] < 0)
+                        waitingTime[shortestProcIndex] = 0;
                 }
                 // Increment time 
-                t++;
+                totalRunningTime++;
             }
+
             for (int i = 0; i < simulator.NumProcBigThanZero; i++)
             {
-                turnAroundTime += waitingTime[i];
+                turnAroundTime += waitingTime[i]+ simulator.ProcessArrivingTime[i].Item2;
             }
             this.PrintResult("SJF", turnAroundTime, simulator.NumProc);
         }
