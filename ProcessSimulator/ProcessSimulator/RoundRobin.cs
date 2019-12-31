b9fb266b2006a4f59f34c4b5643dbe63896c5a00
razs9744 <razs9744@gmail.com>
2019-12-31 16:52:29 +0200

    Add files via upload

--- ProcessSimulator/ProcessSimulator/RoundRobin.cs
+++ ProcessSimulator/ProcessSimulator/RoundRobin.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using NGenerics.DataStructures.Queues;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
 
 namespace ProcessSimulator
 {
@@ -10,7 +13,57 @@ namespace ProcessSimulator
     {
         public override void Schedule(SimulatorModel simulator)
         {
-            throw new NotImplementedException();
+            // sort all the proceeses
+            Array.Sort(simulator.ProcessArrivingTime, (x, y) => x.Item1.CompareTo(y.Item1));
+
+            //array initialize 
+            int[] restBurstTime = new int[simulator.NumProcBigThanZero];
+
+            //var's init
+            int currentTime = simulator.ProcessArrivingTime[0].Item1;
+            const int timeQuantom = 2;
+            int activeProcesses = simulator.NumProcBigThanZero;
+            bool isProcessFinished = false;
+
+            for (int i = 0; i < simulator.NumProcBigThanZero; i++)
+            {
+                restBurstTime[i] = simulator.ProcessArrivingTime[i].Item2;
+            }
+
+            for (int i = 0; activeProcesses > 0;)
+            {
+                if (restBurstTime[i] <= timeQuantom && restBurstTime[i] > 0)
+                {
+                    currentTime += restBurstTime[i];
+                    restBurstTime[i] = 0;
+                    isProcessFinished = true;
+                }
+                else if (restBurstTime[i] > 0)
+                {
+                    restBurstTime[i] = restBurstTime[i] - timeQuantom;
+                    currentTime = currentTime + timeQuantom;
+                }
+                if (restBurstTime[i] == 0 && isProcessFinished)
+                {
+                    activeProcesses--;
+                    turnAroundTime += currentTime - simulator.ProcessArrivingTime[i].Item1;
+                    isProcessFinished = false;
+                }
+                //end of the cycle
+                if (i == simulator.NumProcBigThanZero - 1)
+                {
+                    i = 0;
+                }
+                else if (simulator.ProcessArrivingTime[i + 1].Item1 <= currentTime) //continue to next process if arrived
+                {
+                    i++;
+                }
+                else //back to the beginning if new process ain't arrived
+                {
+                    i = 0;
+                }
+            }
+            this.PrintResult("Round Robin", turnAroundTime, simulator.NumProc);
         }
     }
 }
--- ProcessSimulator/ProcessSimulator/SJF.cs
+++ ProcessSimulator/ProcessSimulator/SJF.cs
@@ -10,15 +10,82 @@ namespace ProcessSimulator
     {
         public override void Schedule(SimulatorModel simulator)
         {
-            int lastBurstTime = 0;
             //sort in descending order 
-            Array.Sort(simulator.ProcessArrivingTime, (x, y) => x.Item2.CompareTo(y.Item2));
+            Array.Sort(simulator.ProcessArrivingTime, (x, y) => x.Item1.CompareTo(y.Item2));
+
+            int currentTime = simulator.ProcessArrivingTime[0].Item1;
+            int[] restBurstTime = new int[simulator.NumProcBigThanZero];
+            int[] waitingTime = new int[simulator.NumProcBigThanZero];
+
+
 
             for (int i = 0; i < simulator.NumProcBigThanZero; i++)
-            {
-                if (i > 0) { lastBurstTime += simulator.ProcessArrivingTime[i - 1].Item2; }
+                restBurstTime[i] = simulator.ProcessArrivingTime[i].Item2;
 
-                turnAroundTime += lastBurstTime + simulator.ProcessArrivingTime[i].Item2;
+            int complete = 0, t = simulator.ProcessArrivingTime[0].Item1, minm = int.MaxValue;
+            int shortest = 0, finish_time;
+            bool check = false;
+
+            // Process until all processes gets 
+            // completed 
+            while (complete != simulator.NumProcBigThanZero)
+            {
+                // Find process with minimum 
+                // remaining time among the 
+                // processes that arrives till the 
+                // current time` 
+                for (int j = 0; j < simulator.NumProcBigThanZero; j++)
+                {
+                    if ((simulator.ProcessArrivingTime[j].Item1 <= t) &&
+                    (restBurstTime[j] < minm) && restBurstTime[j] > 0)
+                    {
+                        minm = restBurstTime[j];
+                        shortest = j;
+                        check = true;
+                    }
+                }
+
+                if (check == false)
+                {
+                    t++;
+                    continue;
+                }
+
+                // Reduce remaining time by one 
+                restBurstTime[shortest]--;
+
+                // Update minimum 
+                minm = restBurstTime[shortest];
+                if (minm == 0)
+                    minm = int.MaxValue;
+
+                // If a process gets completely 
+                // executed 
+                if (restBurstTime[shortest] == 0)
+                {
+
+                    // Increment complete 
+                    complete++;
+                    check = false;
+
+                    // Find finish time of current 
+                    // process 
+                    finish_time = t + 1;
+
+                    // Calculate waiting time 
+                    waitingTime[shortest] = finish_time -
+                                simulator.ProcessArrivingTime[shortest].Item2 -
+                                simulator.ProcessArrivingTime[shortest].Item1;
+
+                    if (waitingTime[shortest] < 0)
+                        waitingTime[shortest] = 0;
+                }
+                // Increment time 
+                t++;
+            }
+            for (int i = 0; i < simulator.NumProcBigThanZero; i++)
+            {
+                turnAroundTime += waitingTime[i];
             }
             this.PrintResult("SJF", turnAroundTime, simulator.NumProc);
         }
